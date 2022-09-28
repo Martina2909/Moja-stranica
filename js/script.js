ef51a0619c1814b41e6656f662b6f05f7ed51141
Martina <94403456+Martina2909@users.noreply.github.com>
2022-09-28 18:11:39 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,33 +1,9 @@
 date = new Date();
-        year = date.getFullYear();
-        month = date.getMonth() + 1;
-        day = date.getDate();
-        document.getElementById("danasnji_datum").innerHTML = day + "." + month + "." + year + ".";
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+document.getElementById("danasnji_datum").innerHTML = day + "." + month + "." + year + ".";
 
-
-        
-        var modal = document.getElementById('modal1');
-        var img = document.getElementById('aquaPark');
-        var modalImg = document.getElementById("slika1");
-        img.onclick = function(){
-          modal.style.display = "block";
-          modalImg.src = this.src;
-        }
-        
-        
-        var span = document.getElementsByClassName("zatvori")[0];
-        
-        span.onclick = function() { 
-          modal.style.display = "none";
-        }
-      
-        
-        var modal = document.getElementById('modal2');
-        var img = document.getElementById('bf');
-        var modalImg = document.getElementById("slika2");
-        img.onclick = function(){
-          modal.style.display = "block";
-          modalImg.src = this.src;
-        }
-        
-     
+function myFunction(x) {
+        x.classList.toggle("change");
+      }
